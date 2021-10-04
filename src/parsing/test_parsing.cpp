210c51c3bf6c696b00a4928489f7870e60c2e09c
ouram <mboivin@student.42.fr>
2021-10-04 14:09:53 +0200

    Fix trailing parsing

--- src/parsing/test_parsing.cpp
+++ src/parsing/test_parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 12:25:08 by mboivin           #+#    #+#             */
-/*   Updated: 2021/10/04 13:52:12 by mboivin          ###   ########.fr       */
+/*   Updated: 2021/10/04 14:05:41 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "Parser.hpp"
 #include "Message.hpp"
 
-// Test parsing use cases
+// Compare Messages and display only values that differe
 
 static int	cmp_msg(const ft_irc::Message& msg, const ft_irc::Message& expected)
 {
@@ -26,12 +26,6 @@ static int	cmp_msg(const ft_irc::Message& msg, const ft_irc::Message& expected)
 	{
 		std::cout << "   Message command:  " << msg.getCommand()
 				  << "\n-> Expected command: " << expected.getCommand() << std::endl;
-		ret = 0;
-	}
-	if (msg.getResponse() != expected.getResponse())
-	{
-		std::cout << "   Message response:  " << msg.getResponse()
-				  << "-> Expected response: " << expected.getResponse() << std::endl;
 		ret = 0;
 	}
 	if (msg.getParams() != expected.getParams())
@@ -46,33 +40,74 @@ static int	cmp_msg(const ft_irc::Message& msg, const ft_irc::Message& expected)
 		std::cout << std::endl;
 		ret = 0;
 	}
+	if (msg.getResponse() != expected.getResponse())
+	{
+		std::cout << "   Message response:  " << msg.getResponse()
+				  << "-> Expected response: " << expected.getResponse() << std::endl;
+		ret = 0;
+	}
 	return (ret);
 }
+
+// Test parsing use cases
 
 int	test_parsing()
 {
 	ft_irc::Parser		parser;
 	ft_irc::IRCClient	dummy_client;
 	ft_irc::Message		msg(dummy_client);
-	ft_irc::Message		expected(dummy_client);
 
+	// Set dummy values to client
 	dummy_client.setNick("nick");
 	dummy_client.setUsername("username");
 
+	// Wrong command format is silently ignored
 	{
+		ft_irc::Message	expected(dummy_client);
+
+		msg = parser.parseMessage("123", dummy_client);
+		assert(cmp_msg(msg, expected));
+	}
+
+	// Unknown command
+	{
+		ft_irc::Message	expected(dummy_client);
+
 		msg = parser.parseMessage("foo", dummy_client);
 		expected.setCommand("foo");
 		expected.setResponse(":irc.42.fr 421 foo :Unknown command\r\n");
 		assert(cmp_msg(msg, expected));
 	}
 
+	// Uppercase command if valid
 	{
+		ft_irc::Message	expected(dummy_client);
+
+		msg = parser.parseMessage("PArt", dummy_client);
+		expected.setCommand("PART");
+		assert(cmp_msg(msg, expected));
+	}
+
+	{
+		ft_irc::Message	expected(dummy_client);
+
+		msg = parser.parseMessage("PRIVMSG :long_trailing :foo : foo", dummy_client);
+		expected.setCommand("PRIVMSG");
+		expected.setParam(":long_trailing :foo : foo");
+		assert(cmp_msg(msg, expected));
+	}
+
+	{
+		ft_irc::Message	expected(dummy_client);
+
 		msg = parser.parseMessage("QUIT", dummy_client);
 		expected.setCommand("QUIT");
 		assert(cmp_msg(msg, expected));
 	}
 
 	{
+		ft_irc::Message	expected(dummy_client);
+
 		msg = parser.parseMessage("NICK foo", dummy_client);
 		expected.setCommand("NICK");
 		expected.setParam("foo");
