b5f4299204bc5c7015e755e8fd771f266f964fcf
ouram <mboivin@student.42.fr>
2021-10-04 14:27:15 +0200

    Restore main

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 13:07:21 by mboivin           #+#    #+#             */
-/*   Updated: 2021/10/04 13:30:56 by mboivin          ###   ########.fr       */
+/*   Updated: 2021/10/04 14:26:56 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,26 @@
 
 int	main( int argc, char** argv )
 {
-	static_cast<void>(argc);
-	static_cast<void>(argv);
-
-	return (test_parsing());
-	// try
-	// {
-	// 	ft_irc::CLIParser CLI_parser(argc, argv);
-	// 	try
-	// 	{
-	// 		ft_irc::IRCServer server(CLI_parser.getBindAddress(),
-	// 		CLI_parser.getPort(), CLI_parser.getPassword());
-	// 		server.run();
-	// 	}
-	// 	catch (std::runtime_error &e)
-	// 	{
-	// 		std::cerr << e.what() << std::endl;
-	// 		return EXIT_FAILURE;
-	// 	}
-	// }
-	// catch (std::runtime_error& e)
-	// {
-	// 	std::cerr << e.what() << std::endl;
-	// 	displayUsage(argv[0]);
-	// 	return EXIT_FAILURE;
-	// }
-	// return (EXIT_SUCCESS);
+	try
+	{
+		ft_irc::CLIParser CLI_parser(argc, argv);
+		try
+		{
+			ft_irc::IRCServer server(CLI_parser.getBindAddress(),
+			CLI_parser.getPort(), CLI_parser.getPassword());
+			server.run();
+		}
+		catch (std::runtime_error &e)
+		{
+			std::cerr << e.what() << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+	catch (std::runtime_error& e)
+	{
+		std::cerr << e.what() << std::endl;
+		displayUsage(argv[0]);
+		return EXIT_FAILURE;
+	}
+	return (EXIT_SUCCESS);
 }
