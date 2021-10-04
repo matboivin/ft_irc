5d47eccb1fa633f139ac215d63888e00cd9ebc19
ouram <mboivin@student.42.fr>
2021-10-04 16:57:35 +0200

    Remove tmp tests from main

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 13:07:21 by mboivin           #+#    #+#             */
-/*   Updated: 2021/10/04 16:40:54 by mboivin          ###   ########.fr       */
+/*   Updated: 2021/10/04 16:56:22 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,29 +23,6 @@ int	main( int argc, char** argv )
 		{
 			ft_irc::IRCServer server(CLI_parser.getBindAddress(),
 			CLI_parser.getPort(), CLI_parser.getPassword());
-
-			// server.addChannel("general");
-			// ft_irc::IRCClient	foo;
-			// ft_irc::IRCClient	bar;
-
-			// foo.setNick("Foo");
-			// bar.setNick("Bar");
-
-			// std::cout << std::boolalpha
-			// 		  << "Foo in channel general? " << server.userInChannel(foo, "general") << '\n'
-			// 		  << "Bar in channel general? " << server.userInChannel(bar, "general") << '\n';
-
-			// server.addUserToChannel(foo, "general");
-			// server.addUserToChannel(bar, "general");
-
-			// std::cout << "Foo in channel general? " << server.userInChannel(foo, "general") << '\n'
-			// 		  << "Bar in channel general? " << server.userInChannel(bar, "general") << '\n';
-
-			// server.removeUserFromChannel(bar, "general");
-
-			// std::cout << "Foo in channel general? " << server.userInChannel(foo, "general") << '\n'
-			// 		  << "Bar in channel general? " << server.userInChannel(bar, "general") << '\n';
-
 			server.run();
 		}
 		catch (std::runtime_error &e)
