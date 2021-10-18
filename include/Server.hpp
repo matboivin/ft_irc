805d462f66c2078e22b4892b0a4eec7b4cf6103e
Mehdi Benjelloun <mbenjell@student.42.fr>
2021-10-18 13:59:23 +0200

    Clean code 🧹 in progress

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 17:37:43 by root              #+#    #+#             */
-/*   Updated: 2021/10/09 17:59:34 by mboivin          ###   ########.fr       */
+/*   Updated: 2021/10/18 13:58:56 by mbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,100 +26,88 @@ namespace ft_irc
 
 	class Server
 	{
-	public:
-		// aliases
-		typedef void (Server::*cmd_fun)(Message& msg); // pointer to command function
-		typedef std::map<std::string, cmd_fun>	cmds_map; // commands functions
+		public:
+			typedef void (Server::*cmd_fun)(Message& msg);
+			typedef std::map<std::string, cmd_fun>	cmds_map;
 
-	private:
-		// attributes
-		std::string			_hostname;
-		std::string			_bind_address;
-		std::string			_port;
-		std::string			_password;
-		//Structure describing an Internet socket address.
-		struct sockaddr_in	_address;
-		//Socket descriptor.
-		int					_sockfd;
-		int					_backlog_max;
-		Parser				_parser;
-		cmds_map			_commands;
-		std::list<Client>	_clients;
-		std::list<Channel>	_channels;
+			Server(std::string bind_address="0.0.0.0",
+				   std::string port="6697",
+				   std::string password="",
+				   std::string hostname="irc.42.fr",
+				   int backlog_max=5);
+			Server(const Server& other);
+			Server&		operator=(const Server& other);
+			~Server();
 
-	public:
-		// constructor
-					Server(std::string bind_address="0.0.0.0",
-						   std::string port="6697",
-						   std::string password="",
-						   std::string hostname="irc.42.fr",
-						   int backlog_max=5);
-		// copy constructor
-					Server(const Server& other);
-		// assignment operator
-		Server&		operator=(const Server& other);
-		// destructor
-					~Server();
+			/* getters and setters */
+			std::string						getBindAddress() const;
+			std::string						getPort() const;
+			std::string						getPassword() const;
+			cmds_map						getCommands() const;
 
-		// getters
-		std::string	getBindAddress() const;
-		std::string	getPort() const;
-		std::string	getPassword() const;
-		cmds_map	getCommands() const;
+			std::list<Client>::iterator		getClient(const std::string& nick);
+			std::list<Channel>::iterator	getChannel(const std::string& chan_name);
 
-		std::list<Client>::iterator		getClient(const std::string& nick);
-		std::list<Channel>::iterator	getChannel(const std::string& chan_name);
+			void							setBindAddress(const std::string& bind_address);
+			void							setPort(const std::string& port);
+			void							setPassword(const std::string& password);
 
-		// setters
-		void		setBindAddress(const std::string& bind_address);
-		void		setPort(const std::string& port);
-		void		setPassword(const std::string& password);
+			/* main loop */
+			int								run();
 
-		// main loop
-		int			run();
+			/* Commands */
+			void							exec_pass_cmd(Message& msg);
+			void							exec_nick_cmd(Message& msg);
+			void							exec_quit_cmd(Message& msg);
+			void							exec_notice_cmd(Message& msg);
+			void							exec_privmsg_cmd(Message& msg);
+		private:
+			std::string						_hostname;
+			std::string						_bind_address;
+			std::string						_port;
+			std::string						_password;
 
-		// Commands
-		void		exec_pass_cmd(Message& msg);
-		void		exec_nick_cmd(Message& msg);
-		void		exec_quit_cmd(Message& msg);
-		void		exec_notice_cmd(Message& msg);
-		void		exec_privmsg_cmd(Message& msg);
+			/* Structure describing an Internet socket address. */
+			struct sockaddr_in				_address;
 
-	private:
-		//Function to create a socket.
-		//create a new listening tcp s	ocket and bind it to the given address and port
-		//https://www.geeksforgeeks.org/socket-programming-cc/
-		bool		_createSocket();
-		int			_sockGetLine(int sockfd, std::string& line);
-		int			_sockGetLine(int sockfd, std::string& line, std::size_t max_bytes);
-		//awaitConnection
-		bool		_awaitNewConnection();
-		bool		_hasPendingConnections();
-		bool		_processClients();
-		int			_disconnectClient(Client& client);
+			/* Socket descriptor. */
+			int								_sockfd;
+			int								_backlog_max;
+			Parser							_parser;
+			cmds_map						_commands;
+			std::list<Client>				_clients;
+			std::list<Channel>				_channels;
 
-		// parsing
-		Message		_parse(Client& sender, const std::string& cmd);
+			bool							_createSocket();
+			int								_sockGetLine(int sockfd, std::string& line);
+			int								_sockGetLine(int sockfd, std::string& line, std::size_t max_bytes);
+			bool							_awaitNewConnection();
+			bool							_hasPendingConnections();
+			bool							_processClients();
+			int								_disconnectClient(Client& client);
 
-		// commands execution
-		void		_init_commands_map();
-		int			_executeCommand(Message& msg);
-		// command response
-		void		_setResponseRecipients(Message& msg);
-		void		_sendResponse(Message& msg);
+			Message							_parse(Client& sender, const std::string& cmd);
 
-		// Channel operations
-		Channel&	_addChannel(const std::string& name);
-		void		_removeChannel(std::list<Channel>::iterator channel);
-		bool		_userOnChannel(Client& client, Channel& channel);
-		bool		_userOnChannel(Client& client, const std::string& chan_name);
-		void		_addUserToChannel(Client& client, Channel& channel);
-		void		_removeUserFromChannel(Client& client, Channel& channel);
+			/* commands execution */
+			void							_init_commands_map();
+			int								_executeCommand(Message& msg);
 
-		// debug
-		int			_sendList(Client& client);
-		int			_sendError(Client& client, const std::string& error);
-	};
+			/* command response */
+			void							_setResponseRecipients(Message& msg);
+			void							_sendResponse(Message& msg);
+
+			/* Channel operations */
+			Channel&						_addChannel(const std::string& name);
+			void							_removeChannel(std::list<Channel>::iterator channel);
+			bool							_userOnChannel(Client& client, Channel& channel);
+			bool							_userOnChannel(Client& client, const std::string& chan_name);
+			void							_addUserToChannel(Client& client, Channel& channel);
+			void							_removeUserFromChannel(Client& client, Channel& channel);
+
+			/* debug */
+			int								_sendList(Client& client);
+			int								_sendError(Client& client, const std::string& error);
+		};
 }
 
-#endif // !SERVER_HPP
+#endif
